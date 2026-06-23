7cedf787f26910b6b8faa5328befbc3bfb79d39b
Colin Lacy <colacy@cisco.com>
2026-06-23 09:53:52 -0400

    demo mostly fixed

--- platform/scripts/03-install-promises.sh
+++ platform/scripts/03-install-promises.sh
@@ -9,13 +9,13 @@ source_generated_env
 require_kubectl_context
 
 [[ -n "${REDIS_PIPELINE_IMAGE:-}" ]] || fail "REDIS_PIPELINE_IMAGE is not set; run 02-build-and-push-images.sh first"
-[[ -n "${RUNTIME_WORKLOAD_PIPELINE_IMAGE:-}" ]] || fail "RUNTIME_WORKLOAD_PIPELINE_IMAGE is not set; run 02-build-and-push-images.sh first"
+[[ -n "${APPLICATION_RELEASE_PIPELINE_IMAGE:-}" ]] || fail "APPLICATION_RELEASE_PIPELINE_IMAGE is not set; run 02-build-and-push-images.sh first"
 
 kubectl create namespace "${DEMO_NAMESPACE}" --dry-run=client -o yaml | kubectl apply -f -
 kubectl create namespace "${CATALOG_NAMESPACE}" --dry-run=client -o yaml | kubectl apply -f -
 
 REDIS_PROMISE="${BUILD_DIR}/redis-promise.yaml"
-RUNTIME_WORKLOAD_PROMISE="${BUILD_DIR}/runtime-workload-promise.yaml"
+APPLICATION_RELEASE_PROMISE="${BUILD_DIR}/application-release-promise.yaml"
 
 render_template \
   "${PLATFORM_DIR}/kratix/promises/redis/promise.yaml.tmpl" \
@@ -23,9 +23,9 @@ render_template \
   REDIS_PIPELINE_IMAGE "${REDIS_PIPELINE_IMAGE}"
 
 render_template \
-  "${PLATFORM_DIR}/kratix/promises/runtime-workload/promise.yaml.tmpl" \
-  "${RUNTIME_WORKLOAD_PROMISE}" \
-  RUNTIME_WORKLOAD_PIPELINE_IMAGE "${RUNTIME_WORKLOAD_PIPELINE_IMAGE}" \
+  "${PLATFORM_DIR}/kratix/promises/application-release/promise.yaml.tmpl" \
+  "${APPLICATION_RELEASE_PROMISE}" \
+  APPLICATION_RELEASE_PIPELINE_IMAGE "${APPLICATION_RELEASE_PIPELINE_IMAGE}" \
   CATALOG_NAMESPACE "${CATALOG_NAMESPACE}"
 
 log "Installing Redis Promise"
@@ -33,7 +33,7 @@ kubectl apply -f "${REDIS_PROMISE}"
 wait_for_crd redis.runtimeconditions.io
 
 log "Installing RuntimeWorkload Promise"
-kubectl apply -f "${RUNTIME_WORKLOAD_PROMISE}"
+kubectl apply -f "${APPLICATION_RELEASE_PROMISE}"
 wait_for_crd runtimeworkloads.runtimeconditions.io
 
 log "Runtime Conditions Promises are installed"
